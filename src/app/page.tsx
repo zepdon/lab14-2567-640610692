b2a427d3f7fa2a55ce9ff2e01d4714bf0db8b76b
Poon <zeepengineer@gmail.com>
2024-09-06 11:13:55 +0700

    finished????

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,45 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import { Container, Rating, Text, Title, Textarea, Button, Divider, Group, Pagination } from "@mantine/core";
 
 export default function Home() {
   return (
     <Container size="600px">
-      <Title order={2}>Food Review 🍕</Title>
+      <Title order={2} mb="xs">Food Review 🍕</Title>
 
-      <Text ta="center" my="sm">
-        Copyright © 2024 Chayanin Suatap 650610560
+      <Title order={4}>Your Rating</Title>
+      <Rating defaultValue={0} mb="xs"/>
+      <Textarea
+        label="Your Review"
+        // description="pls rate our food UwU"
+        placeholder="Do you enjoy eating?"
+        mb="xs"
+        autosize
+        minRows={3}
+        maxRows={3}
+      />
+      <Button variant="filled" color="orange">Submit Review</Button>
+
+      <Divider my="md" />
+      <Group justify="center">
+      <Title order={3} >Elon Musk</Title>
+      <Rating defaultValue={5} />
+      </Group>
+      <Text size="sm" c="gray" ta="center">Best pizza in this wold. I give you X score</Text>
+      
+      <Divider my="md" />
+      <Group justify="center" >
+      <Title order={3} >Mark Zuck</Title>
+      <Rating defaultValue={4} />
+      </Group>
+      <Text size="sm" c="gray" ta="center" mb="xs">My facourite part is pepperoni</Text>
+
+      <Pagination total={20} color="orange" mt="lg"/>
+
+
+      <Text ta="center" my="sm" c="gray">
+        Copyright © 2024 Poonnapat Panumonwatee 640610692
       </Text>
+      
     </Container>
   );
 }
